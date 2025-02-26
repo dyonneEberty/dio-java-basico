8c38bb5f0cc3e9cf8fef2861db2aa7b98dba71a5
dyonneEberty <bydyonne@gmail.com>
2025-02-26 10:04:45 -0300

    Implementação da mensagem de exibição para o usuario

--- src/main/java/org/example/ContaTerminal.java
+++ src/main/java/org/example/ContaTerminal.java
@@ -12,19 +12,20 @@ public class ContaTerminal {
         String nomeCliente;
         Double saldo;
 
-        System.out.println("Digite o número da conta: ");
+        System.out.println("Por favor digite o número da conta: ");
         numero = scanner.nextInt();
 
-        System.out.println("Digite a agencia bancaria: ");
+        System.out.println("Por favor digite a agencia bancaria: ");
         agencia = scanner.next();
 
-        System.out.println("Digite o nome do cliente: ");
+        System.out.println("Por favor digite o nome do cliente: ");
         nomeCliente = scanner.next();
 
-        System.out.println("Digite o saldo bancario: ");
+        System.out.println("Por favor digite o saldo bancario: ");
         saldo = scanner.nextDouble();
 
-        System.out.println("Numero: " +numero+ " - Agengia: " +agencia+ " - Nome do cliente: " +nomeCliente+ " - Saldo: " +saldo);
+        System.out.println("Olá " +nomeCliente+ ", obrigado por criar uma conta em nosso banco, sua agência é "
+                +agencia+ ", conta " +numero+ ", e seu saldo " +saldo+ " já está disponível para saque!");
 
     }
 }
